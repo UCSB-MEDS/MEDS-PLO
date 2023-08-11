a89451f3e7083473f55e45be4f70b80e0e81f9c8
samanthacsik <samanthacsik@gmail.com>
2023-08-11 15:21:17 -0700

    q17 & 18

--- R/q17_linear_regression.R
+++ R/q17_linear_regression.R
@@ -4,7 +4,10 @@
 
 clean_q17a_familiar_lr <- function(PLO_data_clean){
   
-  PLO_data_clean |> 
+  # to iterate over ----
+  options <- c("1 - never heard of it", "2", "3 - vague sense of what it means", "4", "5 - very familiar")
+  
+  df1 <- PLO_data_clean |> 
     
   # select necessary cols ----
   select(linear_regression) |> 
@@ -12,11 +15,37 @@ clean_q17a_familiar_lr <- function(PLO_data_clean){
   # sum ----
   group_by(linear_regression) |>
     count() |>
-    ungroup() |> 
+    ungroup() 
+  
+  for (i in 1:length(options)){
     
-  # ADDING BC NO ONE SELECTED THE FOLLOWING OPTIONS ----
-  add_row(linear_regression = "2", n = 0) |>
-  add_row(linear_regression = "1 - never heard of it", n = 0) |>
+    cat_name <- options[i]
+    
+    # if category already exists in df, skip to next one
+    if (cat_name %in% pull(df1[,1])) {
+      
+      message(cat_name, " already exists. Moving to next option.")
+      df1 <- df1
+      
+      # if category doesn't already exist, add it with n = 0 so that it still shows up on plot
+    } else {
+      
+      message(cat_name, " does not exist. Adding now.")
+      new_row <- data.frame(linear_regression = cat_name, n = 0)
+      df1 <- rbind(df1, new_row)
+      
+    }
+    
+    message("----------------------")
+    
+  } 
+    
+  # # ADDING BC NO ONE SELECTED THE FOLLOWING OPTIONS ----
+  # add_row(linear_regression = "2", n = 0) |>
+  # add_row(linear_regression = "1 - never heard of it", n = 0) |>
+  
+  # finish wrangling ----
+  df2 <- df1 |> 
     
   # reorder factors ----
   mutate(linear_regression = fct_relevel(linear_regression, 
